530b4774558756ec1419a3fa6a4d2f44382bff11
hyehye225 <55949875+hyehye225@users.noreply.github.com>
2023-12-23 12:45:33 +0900

    권한 오류 관련 테스트...

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <>
-  <Layout />
-  <h1>프로젝트 시작</h1>
-  </>;
+  return (
+    <>
+      <Layout />
+      <h1>프로젝트 시작</h1>
+      <h1>오류 해결 성공??</h1>
+    </>
+  );
 }
 
 export default App;
--- fe/src/components/Layout/MainHeader.js
+++ fe/src/components/Layout/MainHeader.js
@@ -1,5 +1,4 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
 import { css } from "@emotion/react";
 const MainHeader = (props) => {
   const headerStyles = css`
