353a177598ca72e57e8475c1189e48cc2f1505ec
bjh0524 <bjh2183@naver.com>
2023-12-23 12:20:46 +0900

    임시 테스트

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <Layout />;
+  return <>
+  <Layout />
+  <h1>프로젝트 시작</h1>
+  </>;
 }
 
 export default App;
